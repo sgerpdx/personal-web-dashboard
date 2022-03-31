a08364ef5e23df4138e616a86c6d1f102ed7d3ca
SamuelHGerber <henrylightfoot@gmail.com>
2022-03-31 14:52:42 -0700

    basic Jest testing setup for Nextjs

---
+++ __tests__/index.test.tsx
@@ -0,0 +1,5 @@
+// Refer to docs for setup (section on Jest/RTL): https://nextjs.org/docs/testing
+
+export const genericLog = () => {
+  console.log("Placeholder reporting for duty.");
+};
---
+++ __tests__/snapshot.js
@@ -0,0 +1,9 @@
+// Consider rewriting this file in TypeScript if applicable
+
+import { render } from "@testing-library/react";
+import Home from "../pages/index";
+
+it("renders homepage unchanged", () => {
+  const { container } = render(<Home />);
+  expect(container).toMatchSnapshot();
+});
