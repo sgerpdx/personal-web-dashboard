f1079f8fe2345cc44577bd3e46e08b3e8b41d864
SamuelHGerber <henrylightfoot@gmail.com>
2022-04-02 08:10:41 -0700

    test red for component rendering

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -7,7 +7,11 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
+// Page and component imports
 import Home from "../pages/index";
+import Clock from "../components/Clock";
+import ImageOfDay from "../components/ImageOfDay";
+import ContentList from "../components/content/ContentList";
 
 describe("Home", () => {
   it("renders a greeting message", () => {
@@ -16,5 +20,15 @@ describe("Home", () => {
     const greetingMessage = screen.getByText("Good morning user!");
 
     expect(greetingMessage).toBeInTheDocument();
+  });
+
+  it("renders imported static components", () => {
+    render(<Home />);
+
+    const dateMessage = screen.getByText("current day and date");
+    const contentItem = screen.getByRole("listitem", { name: "news" });
+
+    expect(dateMessage).toBeInTheDocument();
+    expect(contentItem).toBeInTheDocument();
   });
 });
--- components/Clock.tsx
+++ components/Clock.tsx
@@ -6,7 +6,7 @@ export default function Clock() {
       <section>
         <div className="bg-black text-white h-240 w-80">
           <h3>3:45 PM</h3>
-          <p>current day and date</p>
+          <p>current day and date and strawberry</p>
         </div>
       </section>
     </>
