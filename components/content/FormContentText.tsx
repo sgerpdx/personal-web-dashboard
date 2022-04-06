e26c930cdbad6dbe1295c3efa2b7100d0f8445ee
SamuelHGerber <henrylightfoot@gmail.com>
2022-04-06 12:52:17 -0700

    Formik demo setup

---
+++ components/content/FormContentText.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useFormik } from "formik";
+
+export default function FormContentText() {
+  return (
+    <>
+      <div className="bg-green-300">
+        <h2>Formik Form:</h2>
+      </div>
+    </>
+  );
+}
