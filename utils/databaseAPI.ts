69fe9436160ef8e622ca0ed2b57e454379266ede
SamuelHGerber <henrylightfoot@gmail.com>
2022-04-08 11:00:25 -0700

    all CRUD routes and handlers working for bookmarks

--- utils/databaseAPI.ts
+++ utils/databaseAPI.ts
@@ -1,6 +1,7 @@
-// This file contains CRUD-route access to the Dashboard Server
+// This file contains CRUD-route access to the Dashboard Server for the purpose of interfacing with the app-native postgresSQL database
 
 const databaseURL: string = "https://agile-refuge-37723.herokuapp.com/api/v1";
+//const databaseURL: string = "http://localhost:3100/api/v1/";
 
 export async function getFetchBookmarks(url = `${databaseURL}/bookmarks`) {
   const response = await fetch(url, {
@@ -14,12 +15,24 @@ export async function getFetchBookmarks(url = `${databaseURL}/bookmarks`) {
   return response.json();
 }
 
+export async function getSingleBookmark(url = `${databaseURL}/bookmarks/6`) {
+  const response = await fetch(`${url}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      //'Content-Type': 'application/x-www-form-urlencoded'
+    },
+  });
+  console.log("Fetch Test Res:", response.json);
+  return response.json();
+}
+
 export async function addFetchBookmarks(
   url = `${databaseURL}/bookmarks`,
   data = {
-    bookmarkTitle: "The AV Club",
-    bookmarkURL: "https://www.avclub.com/",
-    dateCreated: "2022-04-06",
+    bookmarkTitle: "The Bowling Club",
+    bookmarkURL: "https://www.redbeardbowling.io/",
+    dateCreated: "2022-04-08",
   }
 ) {
   const response = await fetch(url, {
@@ -29,6 +42,39 @@ export async function addFetchBookmarks(
       //'Content-Type': 'application/x-www-form-urlencoded'
     },
     body: JSON.stringify(data),
+  });
+  console.log("Fetch Test Res:", response.json);
+  return response.json();
+}
+
+export async function updateBookmark(
+  url = `${databaseURL}/bookmarks/6`,
+  data = {
+    id: 6,
+    bookmarkTitle: "The AV Yacht Club",
+    bookmarkURL: "https://www.avclub.com/",
+    dateCreated: "2022-04-06",
+  }
+) {
+  const response = await fetch(`${url}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      //'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: JSON.stringify(data),
+  });
+  console.log("Fetch Test Res:", response.json);
+  return response.json();
+}
+
+export async function deleteBookmark(url = `${databaseURL}/bookmarks/7`) {
+  const response = await fetch(`${url}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      //'Content-Type': 'application/x-www-form-urlencoded'
+    },
   });
   console.log("Fetch Test Res:", response.json);
   return response.json();
---
+++ utils/externalAPI.ts
@@ -0,0 +1,20 @@
+// This file contains fetch/GET access to the Dashboard Server for the purpose of hitting app-external APIs
+
+const serverURL: string = "https://agile-refuge-37723.herokuapp.com/api/v1";
+//const databaseURL: string = "http://localhost:3100/api/v1/";
+
+export async function getNews(url = `${serverURL}/news`) {
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        //'Content-Type': 'application/x-www-form-urlencoded'
+      },
+    });
+    console.log("News Fetch Res:", response.json);
+    return response.json();
+  } catch (error: any) {
+    if (error) return error.message;
+  }
+}
