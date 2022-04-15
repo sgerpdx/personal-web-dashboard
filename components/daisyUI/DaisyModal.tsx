921281f2fc23955517b47e5adb502e99cbfc46e9
SamuelHGerber <henrylightfoot@gmail.com>
2022-04-15 11:52:01 -0700

    next deploy fixes

--- components/daisyUI/DaisyModal.tsx
+++ components/daisyUI/DaisyModal.tsx
@@ -17,8 +17,8 @@ export default function DaisyModal() {
             Congratulations random Internet user!
           </h3>
           <p className="py-4">
-            You've been selected for a chance to get one year of subscription to
-            use Wikipedia for free!
+            You have been selected for a chance to get one year of subscription
+            to use Wikipedia for free!
           </p>
           <div className="modal-action">
             <label htmlFor="my-modal" className="btn">
