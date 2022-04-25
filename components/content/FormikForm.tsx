90b8c20f308c9d9cca85cf0fa2d330ec482c4a9d
SamuelHGerber <henrylightfoot@gmail.com>
2022-04-25 15:01:11 -0700

    modular map developed for FormikForm component

--- components/content/FormikForm.tsx
+++ components/content/FormikForm.tsx
@@ -5,35 +5,49 @@ export default function FormikForm() {
   // Mock props:
   const formLabel = "Demographic Information";
   const inputQuantity = 3;
-  const inputFormStyle = "divStyle";
+  const formStyle = "formStyle";
+  const inputStyle = "inputStyle";
   const placeholderText = "enter here";
-  const inputOne = {
-    labelOne: "Page Title",
-    keywordOne: "title",
-    typeOne: "text",
-  };
-  //const { labelOne, keywordOne, typeOne } = inputOne;
-  const inputTwo = {
-    labelTwo: "Email Address",
-    keywordTwo: "email",
-    typeTwo: "email",
-  };
-  //const { labelTwo, keywordTwo, typeTwo } = inputTwo;
-  const inputThree = {
-    labelThree: "Age",
-    keywordThree: "age",
-    typeThree: "number",
-  };
-  //const { labelThree, keywordThree, typeThree } = inputThree;
+
+  // Input-specific props as single data array to map through
+  // Schema is that indices 0 and 1 are email and password, 2-3 are text
+  const formInputs = [
+    {
+      label: "Email Address",
+      keyword: "email",
+      type: "email",
+      active: true,
+    },
+    {
+      label: "Password",
+      keyword: "password",
+      type: "password",
+      active: true,
+    },
+    {
+      label: "Page Title",
+      keyword: "title",
+      type: "text",
+      active: false,
+    },
+    {
+      label: "Page URL",
+      keyword: "url",
+      type: "text",
+      active: false,
+    },
+  ];
 
   // Local state variables:
+  let counter = 0;
 
-  // Formik setup:
+  // Formik for mapping -- wait this may not be possible with Formik
   const formik = useFormik({
     initialValues: {
-      keywordOne: "",
-      keywordTwo: "",
-      keywordThree: 0,
+      email: "",
+      password: "",
+      textOne: "",
+      textTwo: "",
     },
     onSubmit: (values) => {
       console.log("Current active values:", values);
@@ -41,107 +55,45 @@ export default function FormikForm() {
       // Call whatever database or useState setter functions need to be invoved with the Formik values, e.g. saveBookmark, setVariable (various) etc.
 
       // Reset form values on submit (can call initialValues?)
-      formik.values.keywordOne = "";
-      formik.values.keywordTwo = "";
-      formik.values.keywordThree = 0;
+      formik.values.email = "";
+      formik.values.password = "";
+      formik.values.textOne = "";
+      formik.values.textTwo = "";
     },
   });
 
   return (
-    <div className={inputFormStyle}>
+    <div className={formStyle}>
       <h2 className="text-lg font-bold">{formLabel}</h2>
       <form
         onSubmit={formik.handleSubmit}
         className="flex flex-col justify-center align-middle"
       >
         <div>
-          {" "}
-          <label className="px-4" htmlFor="title">
-            {inputOne.labelOne}:
-          </label>
-          <input
-            type={inputOne.typeOne}
-            id={inputOne.keywordOne}
-            name={inputOne.keywordOne}
-            placeholder={placeholderText}
-            className="input border border-solid border-darkmaroon text-customblack rounded-none w-64 h-8 m-2"
-            onChange={formik.handleChange}
-            value={formik.values.keywordOne}
-          />
+          {formInputs.map((input) => {
+            if (input.active) {
+              const newInput = (
+                <div>
+                  {" "}
+                  <label className="px-4" htmlFor="title">
+                    {input.label}:
+                  </label>
+                  <input
+                    type={input.type}
+                    id={input.keyword}
+                    name={input.keyword}
+                    placeholder={placeholderText}
+                    className={inputStyle}
+                    onChange={formik.handleChange}
+                    value={formik.values[counter]}
+                  />
+                </div>
+              );
+              counter++;
+              return newInput;
+            }
+          })}
         </div>
-
-        {inputQuantity > 1 ? (
-          <div>
-            {" "}
-            <label className="px-4" htmlFor="title">
-              {inputTwo.labelTwo}:
-            </label>
-            <input
-              type={inputTwo.typeTwo}
-              id={inputTwo.keywordTwo}
-              name={inputTwo.keywordTwo}
-              placeholder={placeholderText}
-              className="input border border-solid border-darkmaroon text-customblack rounded-none w-64 h-8 m-2"
-              onChange={formik.handleChange}
-              value={formik.values.keywordTwo}
-            />
-          </div>
-        ) : (
-          <></>
-        )}
-
-        {inputQuantity > 2 ? (
-          <div>
-            {" "}
-            <label className="px-4" htmlFor="title">
-              {inputThree.labelThree}:
-            </label>
-            <input
-              type={inputThree.typeThree}
-              id={inputThree.keywordThree}
-              name={inputThree.keywordThree}
-              placeholder={placeholderText}
-              className="input border border-solid border-darkmaroon text-customblack rounded-none w-64 h-8 m-2"
-              onChange={formik.handleChange}
-              value={formik.values.keywordThree}
-            />
-          </div>
-        ) : (
-          <></>
-        )}
-
-        {/* <div>
-          {" "}
-          <label className="px-4" htmlFor="title">
-            {inputFour.labelFour}:
-          </label>
-          <input
-            type={inputFour.typeFour}
-            id={inputFour.keywordFour}
-            name={inputFour.keywordFour}
-            placeholder={placeholderText}
-            className="input border border-solid border-darkmaroon text-customblack rounded-none w-64 h-8 m-2"
-            onChange={formik.handleChange}
-            value={formik.values.keywordFour}
-          />
-        </div> */}
-
-        {/* <div>
-          {" "}
-          <label className="px-4" htmlFor="title">
-            {inputFive.labelFive}:
-          </label>
-          <input
-            type={inputFive.typeFive}
-            id={inputFive.keywordFive}
-            name={inputFive.keywordFive}
-            placeholder={placeholderText}
-            className="input border border-solid border-darkmaroon text-customblack rounded-none w-64 h-8 m-2"
-            onChange={formik.handleChange}
-            value={formik.values.keywordFive}
-          />
-        </div> */}
-
         <div className="flex flex-row justify-center align-baseline pt-2">
           {" "}
           <button
